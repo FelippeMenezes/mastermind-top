408c183e10d6daba7d7f5691ab04dc30f57b9b3d
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-26 11:55:24 -0300

    Initial value of the #initialize variables in the Board class have been updated.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 class Board
   attr_accessor :secret_code, :user_guess, :round, :feed_back
 
-  def initialize(secret_code, user_guess, round, feed_back)
+  def initialize(secret_code:, feed_back: [], user_guess: nil, round: 1)
     @secret_code = secret_code
     @user_guess = user_guess
     @round = round
