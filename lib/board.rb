e4d0a93f05d63e82c90af8433c1fb883a768b27e
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-25 22:39:56 -0300

    #initialize created and 'attr_accessor' for the initial variables for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,8 @@
 class Board
-
+  def initialize(position_1, position_2, position_3, position_4)
+    @position_1 = position_1
+    @position_2 = position_2
+    @position_3 = position_3
+    @position_4 = position_4
+  end
 end
